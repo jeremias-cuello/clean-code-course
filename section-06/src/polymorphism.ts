cc0e2e5463ad1f7a6b130e0e9db9a0e9a50bc8f1
Jeremias <cuellojeremiasnatanael@gmail.com>
2026-03-06 20:12:07 -0300

    performing code base

--- section-06/src/polymorphism.ts
+++ section-06/src/polymorphism.ts
@@ -1,9 +1,40 @@
-type Purchase = any;
 
-let Logistics: any;
+type Purchase = {
+    product: string,
+    deliveryType: string,
+};
+
+function trackPurchase(product: string) {
+    console.log(`Track product: ${product}`);
+}
+
+function issuePurchase(product: string) {
+    console.log(`Track product: ${product}`);
+}
+
+let Logistics: any = {
+    trackExpressDelivery(product: string) {
+        trackPurchase(product);
+    },
+    issueExpressDelivery(product: string) {
+        issuePurchase(product);
+    },
+    trackStandardDelivery(product: string) {
+        trackPurchase(product);
+    },
+    issueStandardDelivery(product: string) {
+        issuePurchase(product);
+    },
+    trackInsuredDelivery(product: string) {
+        trackPurchase(product);
+    },
+    issueInsuredDelivery(product: string) {
+        issuePurchase(product);
+    },
+};
 
 class Delivery {
-    private purchase: Purchase;
+    protected purchase: Purchase;
 
     constructor(purchase: Purchase) {
         this.purchase = purchase;
@@ -29,3 +60,67 @@ class Delivery {
         }
     }
 }
+
+// class DeliveryExpress extends Delivery {
+//     deliverProduct(): void {
+//         Logistics.issueExpressDelivery(this.purchase.product);
+//     }
+
+//     trackProduct(): void {
+//         Logistics.trackExpressDelivery(this.purchase.product);
+//     }
+// }
+
+// class DeliveryInsured extends Delivery {
+//     deliveryProduct(): void {
+//         Logistics.issueInsuredDelivery(this.purchase.product);
+//     }
+
+//     trackProduct(): void {
+//         Logistics.trackInsuredDelivery(this.purchase.product);
+//     }
+// }
+
+// class DeliveryStandard extends Delivery {
+//     deliveryProduct(): void {
+//         Logistics.issueStandardDelivery(this.purchase.product);
+//     }
+
+//     trackProduct(): void {
+//         Logistics.trackStandardDelivery(this.purchase.product);
+//     }
+// }
+
+class Main {
+    main() {
+        let purchase: Purchase = {
+            deliveryType: '',
+            product: 'Soda',
+        };
+
+        const deliveryStandard = new Delivery(purchase);
+        deliveryStandard.deliverProduct();
+        deliveryStandard.trackProduct();
+
+        purchase = {
+            deliveryType: 'insured',
+            product: 'Café',
+        };
+
+        const deliveryInsured = new Delivery(purchase);
+        deliveryInsured.deliverProduct();
+        deliveryInsured.trackProduct();
+
+        purchase = {
+            deliveryType: 'express',
+            product: 'Cereal',
+        };
+
+        const deliveryExpress = new Delivery(purchase);
+        deliveryExpress.deliverProduct();
+        deliveryExpress.trackProduct();
+    }
+}
+
+const program = new Main();
+program.main();
